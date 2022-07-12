a0df4fd595ecfda8c10aefe541cfcb2364970911
Adilson Brito <abrito10@gmail.com>
2022-07-12 16:18:17 -0300

    Project created

---
+++ backend/src/test/java/com/abrito10/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrito10.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
