a378d385c3f380d9f6d07f9d77e45a4ee4265ae8
Adilson Brito <abrito10@gmail.com>
2022-07-13 11:03:09 -0300

    API test

---
+++ backend/src/main/java/com/abrito10/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.abrito10.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abrito10.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
